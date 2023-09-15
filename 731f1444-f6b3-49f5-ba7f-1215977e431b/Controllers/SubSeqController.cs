e9e405de299fd7e9fef6a745433ae5084a902fd6
NewKi <kianoush.karbasi@newkitrade.com>
2023-09-15 10:46:22 +1000

    using file instead of directly input numbers

--- 731f1444-f6b3-49f5-ba7f-1215977e431b/Controllers/SubSeqController.cs
+++ 731f1444-f6b3-49f5-ba7f-1215977e431b/Controllers/SubSeqController.cs
@@ -6,19 +6,30 @@ namespace _731f1444_f6b3_49f5_ba7f_1215977e431b.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class SubSeqController : ControllerBase
-{    
+{
     private readonly ILogger<SubSeqController> _logger;
     private readonly ILongestSequenceService _longestService;
 
-    public SubSeqController(ILogger<SubSeqController> logger,ILongestSequenceService longestSequenceService)
+    public SubSeqController(ILogger<SubSeqController> logger, ILongestSequenceService longestSequenceService)
     {
         _logger = logger;
         _longestService = longestSequenceService;
     }
 
-    [HttpGet()]
-    public IEnumerable<int> Get(string numbers)
+    [HttpPost("upload")]
+    public IEnumerable<int> UploadFile(IFormFile file)
     {
-        return _longestService.GetLongestList(numbers);        
+        if (file == null || file.Length == 0)
+        {
+            // Handle the case where no file is provided
+            // You can return an error response or appropriate status code here
+            return new List<int>();
+        }
+
+        using (var reader = new StreamReader(file.OpenReadStream()))
+        {
+            var fileContent = reader.ReadToEnd();
+            return _longestService.GetLongestList(fileContent);
+        }
     }
 }
